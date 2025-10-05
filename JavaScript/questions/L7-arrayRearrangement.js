dc9adcb1068d942b83c6f8b6aec9277547e648f1
Abhishek Sharma <abhi.sharma2800015@gmail.com>
2025-10-05 21:23:35 +0530

    In place transformation

--- JavaScript/questions/L7-arrayRearrangement.js
+++ JavaScript/questions/L7-arrayRearrangement.js
@@ -14,6 +14,7 @@ async function rearrangeArray() {
     arr[i] = userInput;
   }
   bruteForce(arr);
+  efficientSolution(arr);
 }
 
 rearrangeArray();
@@ -25,4 +26,30 @@ function bruteForce(arr) {
     tempArray[arr[i]] = i;
   }
   console.log("BRUTE_FORCE", { tempArray, original: arr });
+}
+
+function efficientSolution(arr) {
+  const arraysize = arr.length;
+  let nextIndex = null;
+
+  for (let i = 0; i < arraysize; i++) {
+    if (arr[i] < 0) {
+      continue;
+    }
+    nextIndex = arr[i];
+    let val = (i + 1) * -1;
+    // loop till we are back at the current index (that will also mean cycle is complete)
+    while (nextIndex !== i) {
+      let temp = arr[nextIndex]; // what the next index would be
+      arr[nextIndex] = val;
+      val = (nextIndex + 1) * -1;
+      nextIndex = temp;
+    }
+    arr[nextIndex] = val;
+  }
+  for (let i = 0; i < arraysize; i++) {
+    arr[i] = arr[i] * -1 - 1;
+  }
+
+  console.log("Transformed :: ", arr);
 }
